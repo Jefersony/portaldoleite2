e651a2ac9ad94413136ce98ea7ec043d77f11a92
Jefersony <jeferson.holanda@ccc.ufcg.edu.br>
2015-11-19 23:59:32 -0300

    Adiciona novas disciplinas, novos temas e novos usuarios.

--- app/Global.java
+++ app/Global.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import models.Disciplina;
 import models.Tema;
+import models.User;
 import models.dao.GenericDAOImpl;
 import play.Application;
 import play.GlobalSettings;
@@ -24,6 +25,7 @@ public class Global extends GlobalSettings {
 			public void invoke() throws Throwable {
 				if(dao.findAllByClassName(Disciplina.class.getName()).size() == 0){
 					criaDisciplinaTemas();
+					criaUsuarios();
 				}
 			}
 		});
@@ -58,6 +60,56 @@ public class Global extends GlobalSettings {
 		si1.addTema(new Tema("Minitestes"));
 		si1.addTema(new Tema("Projeto"));
 		dao.persist(si1);
+		
+		Disciplina p1 = new Disciplina( "Programação 1");
+		p1.addTema(new Tema( "Lógica Procedural"));
+		p1.addTema(new Tema( "Operadores Lógicos"));
+		p1.addTema(new Tema( "Operadores Condicionais"));
+		p1.addTema(new Tema( "Listas"));
+		p1.addTema(new Tema( "Laço For"));
+		p1.addTema(new Tema( "Laço While"));
+		p1.addTema(new Tema( "Dicionários"));
+		p1.addTema(new Tema( "Matrizes"));
+		dao.persist(p1);
+		
+		Disciplina tc = new Disciplina("Teoria da Computação");
+		tc.addTema(new Tema("Autômatos Determinísticos"));
+		tc.addTema(new Tema("Linguagens Regulares"));
+		tc.addTema(new Tema("Autômatos Não Determinísticos"));
+		tc.addTema(new Tema("Autômatos Com Pilha"));
+		tc.addTema(new Tema("Linguagens Livre de Contexto"));
+		tc.addTema(new Tema("Gramáticas"));
+		tc.addTema(new Tema("Linguagens Sensíveis ao Contexto"));
+		tc.addTema(new Tema("Máquina de Turing"));
+		dao.persist(tc);
+		
+		dao.flush();
+	}
+	
+	public void criaUsuarios(){
+		User usuario0 = new User("jeferson@gmail.com","123456", "jeferson"); usuario0.setNome("Jeferson Holanda");
+		User usuario1 = new User("damon@gmail.com","segredo", "damon"); usuario1.setNome("Damon Albarn");
+		User usuario2 = new User("graham@gmail.com","segredo", "graham"); usuario2.setNome("Graham Coxon");
+		User usuario3 = new User("alex@gmail.com","segredo", "alex"); usuario3.setNome("Alex James");
+		User usuario4 = new User("david@gmail.com","segredo", "david"); usuario4.setNome("David Rowntree");
+		User usuario5 = new User("kurt@gmail.com","segredo", "kurt"); usuario5.setNome("Kurt Cobain");
+		User usuario6 = new User("chris@gmail.com","segredo", "chris"); usuario6.setNome("Chris Novoselic");
+		User usuario7 = new User("daven@gmail.com","segredo", "dave"); usuario7.setNome("Dave Growl");
+		User usuario8 = new User("dolores@gmail.com","segredo", "dolores"); usuario8.setNome("Dolores O'Riddan");
+		User usuario9 = new User("nina@gmail.com","segredo", "nina"); usuario9.setNome("Nina Perssom");
+		User usuario10 = new User("alanis@gmail.com","segredo", "alanis"); usuario10.setNome("Alanis Morissette");
+		
+		dao.persist(usuario1);
+		dao.persist(usuario2);
+		dao.persist(usuario3);
+		dao.persist(usuario4);
+		dao.persist(usuario5);
+		dao.persist(usuario6);
+		dao.persist(usuario7);
+		dao.persist(usuario8);
+		dao.persist(usuario9);
+		dao.persist(usuario10);
+		
 		dao.flush();
 	}
 }
