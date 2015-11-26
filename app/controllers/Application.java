7f01ef030422479f469e0a00544ca5cfeb62acbf
Jefersony <jeferson.holanda@ccc.ufcg.edu.br>
2015-11-26 16:58:41 -0300

    Add doc com detalhes do refactor e limpa coments em Applicat.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -479,22 +479,7 @@ public class Application extends Controller {
 				Dica.class.getName(), "discordancias", String.format("%d", maiorDiscordancia));
 		listaFinal = menosConcordadas;
 		int restin = 10 - listaFinal.size();
-//		System.out.println("valor = "+restin);
-//		if( restin > 0 ){
-//			int k = maiorDiscordancia;// a cada iteracao, checa se tem votados com 1 voto a menos
-//			System.out.println("valor de K "+k);
-//			while( restin > 0 ){
-//				List<Dica> menosConcordadas2 = dao.findByAttributeName(
-//						Dica.class.getName(), "discordancias", String.format("%d", k));
-//				int n = menosConcordadas2.size()-1;
-//				while( !menosConcordadas2.isEmpty() && listaFinal.size() < 10){
-//					listaFinal.add(menosConcordadas2.get(n));
-//					n--;
-//					restin --;
-//				}
-//				k--;
-//			}
-//		}
+		
 		int w = 1;
 		if( menosConcordadas.size() < 10 && !menosConcordadas.isEmpty()){
 			List<Dica> menosConcordadas2 = dao.findByAttributeName(
@@ -521,4 +506,6 @@ public class Application extends Controller {
 		List<Disciplina> listaDisciplina = dao.findAllByClassName(Disciplina.class.getName());
 		return ok(views.html.maisdiscordadas.render(listaDisciplina));
 	}
+	
+	
 }
